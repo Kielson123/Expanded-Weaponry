3b357768d19a61f6d686915499271535cb890a41
Gracek12 <gracjanprzymus12@gmail.com>
2024-08-02 14:56:24 +0200

    Hammer recipes

--- src/main/java/com/kielson/datagen/ModRecipeProvider.java
+++ src/main/java/com/kielson/datagen/ModRecipeProvider.java
@@ -5,7 +5,9 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeExporter;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.data.server.recipe.SmithingTransformRecipeJsonBuilder;
 import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.ItemTags;
@@ -24,6 +26,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
     @Override
     public void generate(RecipeExporter exporter) {
         generateDaggers(exporter);
+        generateHammers(exporter);
     }
 
     private void generateDaggers(RecipeExporter exporter){
@@ -62,11 +65,53 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
                 .offerTo(exporter, Identifier.of(MOD_ID, getRecipeName(ModItems.DIAMOND_DAGGER)));
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.NETHERITE_DAGGER).pattern("b").pattern("s")
-                .input('b', Items.NETHERITE_INGOT)
-                .input('s', Items.STICK)
+        SmithingTransformRecipeJsonBuilder.create(Ingredient.ofItems(Items.NETHERITE_UPGRADE_SMITHING_TEMPLATE), Ingredient.ofItems(ModItems.DIAMOND_DAGGER),
+                        Ingredient.ofItems(Items.NETHERITE_INGOT), RecipeCategory.COMBAT, ModItems.NETHERITE_DAGGER)
                 .criterion(hasItem(Items.NETHERITE_INGOT), conditionsFromItem(Items.NETHERITE_INGOT))
+                .criterion(hasItem(ModItems.DIAMOND_DAGGER), conditionsFromItem(ModItems.DIAMOND_DAGGER))
+                .offerTo(exporter, Identifier.of(MOD_ID, getRecipeName(ModItems.NETHERITE_DAGGER) + "_smithing"));
+    }
+
+    private void generateHammers(RecipeExporter exporter){
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.WOODEN_HAMMER).pattern("hhh").pattern("hhh").pattern(" s ")
+                .input('h', ItemTags.PLANKS)
+                .input('s', Items.STICK)
+                .criterion("planks", conditionsFromTag(ItemTags.PLANKS))
                 .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
-                .offerTo(exporter, Identifier.of(MOD_ID, getRecipeName(ModItems.NETHERITE_DAGGER)));
+                .offerTo(exporter, Identifier.of(MOD_ID, getRecipeName(ModItems.WOODEN_HAMMER)));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.STONE_HAMMER).pattern("hhh").pattern("hhh").pattern(" s ")
+                .input('h', ItemTags.STONE_TOOL_MATERIALS)
+                .input('s', Items.STICK)
+                .criterion("stone", conditionsFromTag(ItemTags.STONE_TOOL_MATERIALS))
+                .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
+                .offerTo(exporter, Identifier.of(MOD_ID, getRecipeName(ModItems.STONE_HAMMER)));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.IRON_HAMMER).pattern("hhh").pattern("hhh").pattern(" s ")
+                .input('h', Items.IRON_INGOT)
+                .input('s', Items.STICK)
+                .criterion(hasItem(Items.IRON_INGOT), conditionsFromItem(Items.IRON_INGOT))
+                .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
+                .offerTo(exporter, Identifier.of(MOD_ID, getRecipeName(ModItems.IRON_HAMMER)));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.GOLDEN_HAMMER).pattern("hhh").pattern("hhh").pattern(" s ")
+                .input('h', Items.GOLD_INGOT)
+                .input('s', Items.STICK)
+                .criterion(hasItem(Items.GOLD_INGOT), conditionsFromItem(Items.GOLD_INGOT))
+                .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
+                .offerTo(exporter, Identifier.of(MOD_ID, getRecipeName(ModItems.GOLDEN_HAMMER)));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.DIAMOND_HAMMER).pattern("hhh").pattern("hhh").pattern(" s ")
+                .input('h', Items.DIAMOND)
+                .input('s', Items.STICK)
+                .criterion(hasItem(Items.DIAMOND), conditionsFromItem(Items.DIAMOND))
+                .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
+                .offerTo(exporter, Identifier.of(MOD_ID, getRecipeName(ModItems.DIAMOND_HAMMER)));
+
+        SmithingTransformRecipeJsonBuilder.create(Ingredient.ofItems(Items.NETHERITE_UPGRADE_SMITHING_TEMPLATE), Ingredient.ofItems(ModItems.DIAMOND_HAMMER),
+                        Ingredient.ofItems(Items.NETHERITE_INGOT), RecipeCategory.COMBAT, ModItems.NETHERITE_HAMMER)
+                .criterion(hasItem(Items.NETHERITE_INGOT), conditionsFromItem(Items.NETHERITE_INGOT))
+                .criterion(hasItem(ModItems.DIAMOND_HAMMER), conditionsFromItem(ModItems.DIAMOND_HAMMER))
+                .offerTo(exporter, Identifier.of(MOD_ID, getRecipeName(ModItems.NETHERITE_HAMMER) + "_smithing"));
     }
 }
