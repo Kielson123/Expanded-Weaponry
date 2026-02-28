809107b0f329f7889415fd1b565666a436f99348
Kielson <gracjanprzymus12@gmail.com>
2026-02-28 18:19:53 +0100

    Adjusted weapon balance

--- src/main/java/com/kielson/item/custom/DaggerItem.java
+++ src/main/java/com/kielson/item/custom/DaggerItem.java
@@ -12,6 +12,7 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ToolMaterial;
+import net.minecraft.world.item.component.AttackRange;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
@@ -19,20 +20,22 @@ import net.minecraft.world.level.block.state.BlockState;
 public class DaggerItem extends Item {
     private static final float ATTACK_DAMAGE = 1.0f;
     private static final float ATTACK_SPEED = -1.9f;
+    private static final float ATTACK_RANGE_MIN = 0.0f;
+    private static final float ATTACK_RANGE_MAX = 2f;
 
     public DaggerItem(ToolMaterial toolMaterial, Item.Properties settings) {
         super(settings
                 .sword(toolMaterial, ATTACK_DAMAGE, ATTACK_SPEED)
                 .durability((int) (toolMaterial.durability() * 0.85))
-                .component(DataComponents.ATTRIBUTE_MODIFIERS, createAttributeModifiers(toolMaterial)));
+                .component(DataComponents.ATTRIBUTE_MODIFIERS, createAttributeModifiers(toolMaterial))
+                .component(DataComponents.ATTACK_RANGE, new AttackRange(ATTACK_RANGE_MIN, ATTACK_RANGE_MAX, ATTACK_RANGE_MIN, Math.max(ATTACK_RANGE_MAX, 5), 0.3f, 1f)));
     }
 
     public static ItemAttributeModifiers createAttributeModifiers(ToolMaterial material) {
         return ItemAttributeModifiers.builder()
                 .add(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_ID, ATTACK_DAMAGE + material.attackDamageBonus(), AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND)
                 .add(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_ID, ATTACK_SPEED, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND)
-                .add(Attributes.ENTITY_INTERACTION_RANGE, new AttributeModifier(Identifier.fromNamespaceAndPath(MOD_ID, "dagger"),
-                        -1.75, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND).build();
+                .build();
     }
 
     @Override
--- src/main/java/com/kielson/item/custom/HammerItem.java
+++ src/main/java/com/kielson/item/custom/HammerItem.java
@@ -3,19 +3,27 @@ package com.kielson.item.custom;
 import static com.kielson.KielsonsAPIComponents.TWO_HANDED;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ToolMaterial;
+import net.minecraft.world.item.component.AttackRange;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class HammerItem extends Item {
+    private static final float ATTACK_DAMAGE = 7.0f;
+    private static final float ATTACK_SPEED = -3.25f;
+    private static final float ATTACK_RANGE_MIN = 1.5f;
+    private static final float ATTACK_RANGE_MAX = 3.0f;
+
     public HammerItem(ToolMaterial toolMaterial, Item.Properties settings) {
         super(settings
-                .axe(toolMaterial, 7.0f, -3.5f)
+                .axe(toolMaterial, ATTACK_DAMAGE, ATTACK_SPEED)
                 .durability((int) (toolMaterial.durability() * 1.1))
-                .component(TWO_HANDED, Boolean.TRUE));
+                .component(TWO_HANDED, Boolean.TRUE)
+                .component(DataComponents.ATTACK_RANGE, new AttackRange(ATTACK_RANGE_MIN, ATTACK_RANGE_MAX, ATTACK_RANGE_MIN, Math.max(ATTACK_RANGE_MAX, 5), 0.3f, 1f)));
     }
 
     @Override
